94067c1899e91581af3f54d63b7e75b2127b446f
Arnaud R <arno.royer2004@gmail.com>
2025-12-12 17:07:58 +0100

    Fix: Improve mobile view for Header and SoundCard component

--- src/components/SoundCard.jsx
+++ src/components/SoundCard.jsx
@@ -43,18 +43,12 @@ const SoundCard = ({ sound, isPlaying, volume, onToggle, onVolumeChange }) => {
   }, [isPlaying]);
 
   return (
-    <div
-      title={`Volume: ${volumePercent}%`}
-      className={`
-        relative p-6 rounded-2xl transition-all duration-300 border border-transparent hover:-translate-y-2
-        ${
-          isPlaying
-            ? `${sound.color} shadow-lg scale-105`
-            : "bg-gray-800 hover:bg-gray-700"
-        }
-      `}
-    >
-      <div className="flex flex-col items-center gap-4">
+
+    <div 
+      title={`Volume ${volumePercent}`} 
+      className={`p-6 rounded-2xl duration-300 border border-transparent hover:-translate-y-2 flex flex-col justify-center items-center gap-5 ${isPlaying ? `${sound.color} shadow-lg scale-105` : 'bg-gray-800 hover:bg-gray-700'}`}
+      >
+
         <button
           onClick={() => onToggle(sound.id)} // Καλούμε τη συνάρτηση του App
           className="p-4 rounded-full bg-white/10 hover:bg-white/20 transition-colors"
@@ -65,41 +59,37 @@ const SoundCard = ({ sound, isPlaying, volume, onToggle, onVolumeChange }) => {
           />
         </button>
 
-        <h3 className="text-white font-medium text-lg">{sound.label}</h3>
+        <h3 className="text-white font-medium text-center md:text-lg">{sound.label}</h3>
 
-        <div
-          className={`
-          w-full transition-opacity duration-300 
-          ${isPlaying ? "opacity-100" : "opacity-0 pointer-events-none"}
-        `}
-        >
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={volume}
-            onChange={(e) =>
-              onVolumeChange(sound.id, parseFloat(e.target.value))
-            }
-            className="
-    w-full h-1 
-    bg-gray-600 rounded-lg 
-    appearance-none cursor-pointer 
-    accent-white
-    
-    /* Custom thumb styling using arbitrary variants for cross-browser support */
-    [&::-webkit-slider-thumb]:w-4 
-    [&::-webkit-slider-thumb]:h-4 
-    [&::-webkit-slider-thumb]:bg-white 
-    [&::-webkit-slider-thumb]:rounded-full 
-    [&::-webkit-slider-thumb]:appearance-none 
-    [&::-webkit-slider-thumb]:mt-[-0.15rem] /* Adjust vertical position */
-    [&::-webkit-slider-thumb]:shadow-lg
-"
-          />
-        </div>
-      </div>
+        {isPlaying && 
+          <div className="w-full">
+            <input
+              type="range"
+              min="0"
+              max="1"
+              step="0.01"
+              value={volume}
+              onChange={(e) =>
+                onVolumeChange(sound.id, parseFloat(e.target.value))
+              }
+              className="
+                w-full h-1 
+                bg-gray-600 rounded-lg 
+                appearance-none cursor-pointer 
+                accent-white
+                
+                /* Custom thumb styling using arbitrary variants for cross-browser support */
+                [&::-webkit-slider-thumb]:w-4 
+                [&::-webkit-slider-thumb]:h-4 
+                [&::-webkit-slider-thumb]:bg-white 
+                [&::-webkit-slider-thumb]:rounded-full 
+                [&::-webkit-slider-thumb]:appearance-none 
+                [&::-webkit-slider-thumb]:mt-[-0.15rem] /* Adjust vertical position */
+                [&::-webkit-slider-thumb]:shadow-lg"
+            />
+          </div>
+        }
+
     </div>
   );
 };
